a29fc8fb89f06e37e331653ee5ac59ec19fa75b6
abraga42 <86627490+abraga42@users.noreply.github.com>
2021-07-17 20:17:10 +0200

    Add files via upload

---
+++ Days/C07/ex00/ft_strdup.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abraga <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/17 13:23:00 by abraga            #+#    #+#             */
+/*   Updated: 2021/07/17 18:52:56 by abraga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest = NULL;
+	int		i;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(&src[i]);
+	dest = malloc(sizeof(char) * len + 1);
+	if (dest == NULL)
+		return (0);
+	if (!(src))
+		return (0);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
