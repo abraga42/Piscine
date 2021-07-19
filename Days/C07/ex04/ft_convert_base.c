073e39ef27bc574e038734910572d39f1b9d41f8
abraga42 <86627490+abraga42@users.noreply.github.com>
2021-07-20 00:27:10 +0200

    Add files via upload

--- Days/C07/ex04/ft_convert_base.c
+++ Days/C07/ex04/ft_convert_base.c
@@ -6,17 +6,11 @@
 /*   By: abraga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/18 17:05:35 by abraga            #+#    #+#             */
-/*   Updated: 2021/07/18 22:02:36 by abraga           ###   ########.fr       */
+/*   Updated: 2021/07/20 00:26:51 by abraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write (1, &c, 1);
-}
 
 int	ft_isspace(char c)
 {
@@ -42,14 +36,14 @@ int	ft_check(char *str, char *base)
 
 	i = 0;
 	if (ft_strlen(base) <= 1)
-		return (NULL);
+		return (0);
 	while (base[i])
 	{
 		if (str[i] == base[i])
 			return (i);
 	}
 	i++;
-	return (NULL);
+	return (0);
 }
 
 int	ft_index(char c, char *base)
@@ -69,60 +63,70 @@ int	ft_index(char c, char *base)
 int	ft_atoi_base(char *str, char *base)
 {
 	int	i;
+	int	mult;
 	int	nbr;
 	int	len;
 
 	i = 0;
+	mult = 1;
 	nbr = 0;
 	len = ft_strlen(base);
 	while (ft_isspace(str[i]))
 		i++;
-	if (str[i] == '+' || str[i] == '-')
-		return (0);
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			mult = mult * -1;
+		i++;
+	}
 	while (str[i] && ft_index(str[i], base) != -1)
 	{
 		nbr *= len + ft_index(str[i], base);
 		i++;
 	}
-	return (nbr);
+	return (nbr * mult);
 }
 
-int	ft_itoa_base(char *nbr, char *base)
+char	*ft_itoa_base(int nbr, char *base)
 {
-	unsigned int	i;
-	unsigned int	n;
-	unsigned int	len;
+	long	i;
+	long	count;
+	long	n;
+	long	len;
+	char	*str = NULL;
 
-	i = 0;
+	i = nbr;
+	count = 0;
 	n = 0;
 	len = ft_strlen(base);
-	if (ft_check(base) == 0)
-		return ;
-	if (i < 0)
+	while (i != 0)
 	{
-		n = -nbr[i];
-		ft_putchar('-');
+		i = i / len;
+		count++;
 	}
 	else
-		n = nbr[i];
-	if (n > len - 1)
 	{
-		ft_itoa_base(n / len, base);
-		n %= len;
+		str = malloc(sizeof(char) * count + 1);
+		str[count] = '\0';
 	}
-	return (nbr); // doit retourner un *str
+	while (nbr != 0)
+	{
+		str[count - 1] = (nbr % len) + 48;
+		nbr = nbr / len;
+		count--;
+	}
+	return (str);
 }
 
-char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
-{
-	nbr = malloc(sizeof(int) * (max - min)); // nbr doit tenir dans un int (lui-meme compris entre min et max)
-	if (!nbr)
-		return (NULL);
-	
-	ft_atoi_base(nbr, base_from); // return nbr in decimal
-	ft_itoa_base(nbr, base_to); // have to return *str (here : *nbr)
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to);
+
+	// nbr doit tenir dans un int (lui-meme compris entre min et max)
 
 	// retourne le res de la conversion de *nbr de base_from en base__to
 
-	free(nbr);
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", ft_atoi_base("80", "0123456789abcdef"));
 }
--- Days/C07/ex04/ft_convert_base2.c
+++ Days/C07/ex04/ft_convert_base2.c
@@ -6,10 +6,30 @@
 /*   By: abraga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/18 17:05:33 by abraga            #+#    #+#             */
-/*   Updated: 2021/07/18 17:05:59 by abraga           ###   ########.fr       */
+/*   Updated: 2021/07/20 00:26:53 by abraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+int	ft_isspace(char c);
+int	ft_strlen(char *str);
+int	ft_check(char *str, char *base);
+int	ft_index(char c, char *base);
+int	ft_atoi_base(char *str, char *base);
 
+char	*ft_isnegative(int nbr, char *base, int len, int baselen)
+{
+	long	tmp;
+	char	*str;
+
+	tmp = nbr;
+	temp *= -1;
+	str = (char *) malloc(sizeof(char) * (len + 2));
+	str[0] = '-';
+	str[len + 1] = '\0';
+	while (tmp != 0)
+	{
+		str[len] = base[tmp % baselen];
+	}
+}
