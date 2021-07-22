2bb718736155670b8a9352d88be77f6843317d46
abraga42 <86627490+abraga42@users.noreply.github.com>
2021-07-22 15:32:18 +0200

    Add files via upload

--- Days/C07/ex05/ft_split.c
+++ Days/C07/ex05/ft_split.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_split2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abraga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/20 23:55:11 by abraga            #+#    #+#             */
-/*   Updated: 2021/07/21 23:20:08 by abraga           ###   ########.fr       */
+/*   Created: 2021/07/22 13:04:17 by abraga            #+#    #+#             */
+/*   Updated: 2021/07/22 14:37:45 by abraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,99 @@
 
 int	ft_is_charset(char c, char *charset)
 {
-	int	index;
+	int	i;
 
-	index = 0;
-	while (charset[index])
+	i = 0;
+	while (charset[i])
 	{
-		if (c == charset[index])
+		if (c == charset[i])
 			return (1);
-		index++;
+		i++;
 	}
 	return (0);
+}
+
+int	ft_wordlen(char *str, char *charset, int start)
+{
+	int	count;
+
+	count = 0;
+	while (str[start] && ft_is_charset(str[start], charset) == 1)
+		start++;
+	while (str[start] && ft_is_charset(str[start], charset) == 0)
+	{
+		start++;
+		count++;
+	}
+	return (count);
 }
 
 int	ft_wordcount(char *str, char *charset)
 {
 	int	i;
-	int	count;
+	int	count2;
 
 	i = 0;
-	count = 0;
+	count2 = 0;
 	while (str[i])
 	{
-		while (ft_is_charset(str[i]) == 1)
-			i++;
-		if (str[i])
-			count++;
-		while (ft_is_charset(str[i]) == 0)
+		if (ft_is_charset(str[i], charset) == 0)
+		{
+			count2++;
+			while (str[i] && ft_is_charset(str[i], charset) == 0)
+				i++;
+		}
+		else
 			i++;
 	}
-	return (count);
+	printf("la valeur sans separateur : %d\n", count2);
+	return (count2);
 }
 
-char	*ft_fill(int *tmp_i, char *str, char *charset)
+char	*ft_strndup(char *str, int start, int size)
 {
-	// 1. Compter le nb de char qu'il y aura a malloc pour chaque string
-	// 2. Malloc un char *str
-	// 3. Boucle remplissant le char *str
-	// 4. Return (str)
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	tmp = NULL;
+	tmp = malloc(sizeof(char) * (size + 1));
+	if (!tmp)
+		return (NULL);
+	while (str[start] && i < size)
+	{
+		tmp[i] = str[start + i];
+		i++;
+	}
+	tmp[i] = '\0';
+	return (tmp);
 }
 
 char	**ft_split(char *str, char *charset)
 {
-	char	**tab;
 	int		i;
-	int		index2;
+	int		j;
+	int		count2;
+	char	**tab;
 
-	*tab = NULL;
 	i = 0;
-	index2 = 0;
-	*tab = malloc(sizeof(char *) * 
+	j = 0;
+	count2 = ft_wordcount(str, charset);
+	tab = NULL;
+	tab = malloc(sizeof(char *) * (count2 + 1));
+	if (!tab)
+		return (NULL);
+	while (str[i] && j < count2)
+	{
+		if (ft_is_charset(str[i], charset) == 0)
+		{
+			tab[j++] = ft_strndup(str, i, ft_wordlen(str, charset, i));
+			while (str[i] && ft_is_charset(str[i], charset) == 0)
+				i++;
+		}
+		else
+			i++;
+	}
+	tab[j] = NULL;
+	return (tab);
 }
